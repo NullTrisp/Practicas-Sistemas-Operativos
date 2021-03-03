f8d75afb98b1bdc21780824d9f08364867b2412f
Nicolás <nicocalji@gmail.com>
2021-03-03 18:27:51 +0000

    done

--- process_test_2.c
+++ process_test_2.c
@@ -5,34 +5,23 @@
 
 int main()
 {
-    char aux;
-    aux = getchar();
-    pid_t sonA = fork(), sonB = fork();
-
-    if (sonA > 0 && sonB > 0) //father
-    {
-        puts("Father");
-        printf("%d %d %d\n", getpid(), sonA, sonB);
-        while (wait(NULL) > 0)
-            ;
-    }
-    else if (sonA == 0 && sonB > 0) //sonA
-    {
-        puts("sonA");
-        printf("%d %d %d\n", getpid(), sonA, sonB);
-        //printf("A %c\n", aux);
-        _exit(0);
-    }
-    else if (sonB == 0 && sonA > 0) //sonB
-    {
-        puts("sonB");
-        printf("%d %d %d\n", getpid(), sonA, sonB);
-        //printf("B %c\n", aux);
-        _exit(0);
-    }
-    else
-    {
-        perror("Shitty ass failure ");
+    char aux = getchar();
+    
+    pid_t sonA = fork(), sonB;
+    
+    if(sonA > 0) { //father
+	sonB = fork();
+	if(sonB == 0) { //sonB of father prints uppercase letter
+		if(aux > 97 || aux < 122) {
+		   printf("%c\n", aux - 32);
+		}
+	}
+    } else {  //sonA of father prints next char 
+	if(aux > 122) {
+	  puts("a");
+	} else {
+	printf("%c\n", aux + 1);
+	}
     }
 
     return 0;
