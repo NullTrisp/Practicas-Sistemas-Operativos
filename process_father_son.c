8e0a9f1ab385e77291c096a7aadaf3646eccd62b
Nicolas <nicocalji@gmail.com>
2021-03-03 16:58:30 +0100

    process copy fork()

---
+++ process_father_son.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h> //tipos de datos de linux kernel
+#include <wait.h>
+
+int main()
+{
+    //process id datatype
+    pid_t son = fork(); //proceso hijo  //padre > 0   //copia hijo = 0
+    int n = 5;
+    if (son > 0) //proceso padre
+    {
+        puts("I'm the father and I wait");
+        wait(NULL); //waits for the child process to die
+    }
+    else if (son == 0)
+    {
+        puts("I'm the son and now me voy a tomar por culo");
+        ++n; //n variable copy
+        printf("n copy %d\n", n);
+        return 0;
+    }
+    else
+    {
+        perror("Shitty ass failure ");
+    }
+
+    printf("The son is now dead and the variable is %d \n", n);
+
+    return 0;
+}
