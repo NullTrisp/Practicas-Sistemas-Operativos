f5ded13f9b14c1b6f33a3757a1306ce1c3a9ecc3
Nicolas <nicocalji@gmail.com>
2021-03-10 16:39:35 +0100

    checked father son test

---
+++ process_test_father_son_check.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
+
+int main()
+{
+	puts("Insert char: ");
+	char aux = getchar();
+
+	pid_t sonA, sonB;
+
+	sonA = fork();
+	if (sonA > 0)
+	{ //father create second son
+		sonB = fork();
+	}
+
+	if (sonA > 0 && sonB > 0) //father
+	{
+		while (wait(NULL) > 0)
+			;
+		puts("Sons are dead, father will die");
+	}
+	else if (sonA == 0 && sonB != 0)
+	{ //sonA of father prints next char
+		printf("%c\n", aux + 1);
+		printf("afeg");
+	}
+	else if (sonB == 0 && sonA != 0)
+	{ //sonB of father prints next char
+		printf("%c\n", toupper(aux));
+	}
+
+	return 0;
+}
