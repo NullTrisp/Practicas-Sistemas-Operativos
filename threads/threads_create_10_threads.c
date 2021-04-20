26dcd6cbabd6e9dbd45ad50b97f209eb15a2b5c9
Nicolás Calvache Jimenez <61060757+NullTrisp@users.noreply.github.com>
2021-04-20 21:04:58 +0200

    Update threads_create_10_threads.c

--- threads/threads_create_10_threads.c
+++ threads/threads_create_10_threads.c
@@ -1,32 +1,37 @@
 #include <pthread.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #define LIMIT 5
 
 pthread_t threads[LIMIT];
-int aux;
-int index = 0;
 
-void *print_created(void *args);
+void *print_thread(void *args);
 
 int main(int argc, char const *argv[])
 {
     for (int i = 0; i < LIMIT; i++)
     {
-        pthread_create(&threads[i], NULL, &print_created, i);
-        usleep(100);
+        pthread_create(&threads[i], NULL, &print_thread, i);
+        sleep(1);
     }
-
-    pthread_join(threads[LIMIT - 1], NULL);
+    
+    for (int i = 0; i < LIMIT; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
     return 0;
 }
 
-void *print_created(void *args)
-{
+void *print_thread(void *args)
+{  
     int index = (int *)args;
     printf("He nacido. Soy el hilo %d con ID %u\n", index + 1, (unsigned int)pthread_self());
-    usleep(1000);
-    pthread_join(threads[(LIMIT - 1) - index], NULL);
+    if(index < LIMIT - 1) {
+        sleep(2);
+        pthread_join(threads[index + 1], NULL);
+    }
+    sleep(1);
     printf("El hilo %d con ID %u finaliza\n", index + 1, (unsigned int)pthread_self());
 }
